1487bcc3e540e7ed66c01ff80b37b982230e3363
blaines <github@technologyaz.com>
2010-11-15 20:19:38 -0600

    fix carrierwave for heroku:

--- app/uploaders/asset_uploader.rb
+++ app/uploaders/asset_uploader.rb
@@ -15,6 +15,10 @@ class AssetUploader < CarrierWave::Uploader::Base
   def store_dir
     "monsters/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  
+  def cache_dir
+    "#{RAILS_ROOT}/tmp/uploads"
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
